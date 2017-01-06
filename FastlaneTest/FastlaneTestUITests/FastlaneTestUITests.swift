602cb28f6f241d69fdf72a8e95a8d21e040376bb
Wasin Thonkaew <haxpor@gmail.com>
2017-01-06 20:31:28 +0700

    Add fastlane support to the project
    Added also snapshot, and configured testing on multiple different type
    of devices and languages.
    
    Modified FastFile to include
    - creating a new application data on iTunes connect
    - cert & sigh before uploading to beta testflight

--- FastlaneTest/FastlaneTestUITests/FastlaneTestUITests.swift
+++ FastlaneTest/FastlaneTestUITests/FastlaneTestUITests.swift
@@ -18,7 +18,11 @@ class FastlaneTestUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        
+        // modified to integrate with snapshot
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -34,6 +38,9 @@ class FastlaneTestUITests: XCTestCase {
         firstnametextfieldTextField.typeText("wasin")
 
         XCTAssert(firstnametextfieldTextField.value as! String == "wasin")
+        
+        // take a snapshot
+        snapshot("usernameWasin")
     }
     
     func testLastnameIsThonkaew() {
@@ -42,6 +49,9 @@ class FastlaneTestUITests: XCTestCase {
         lastnametextfieldTextField.typeText("thonkaew")
         
         XCTAssert(lastnametextfieldTextField.value as! String == "thonkaew")
+        
+        // take a snapshot
+        snapshot("lastnameWasin")
     }
     
     func testShowNewPage() {
@@ -57,5 +67,8 @@ class FastlaneTestUITests: XCTestCase {
         backButton = app.buttons["backButton"]
         // check exists
         XCTAssert(backButton.exists)
+        
+        // take a snapshot
+        snapshot("newpage")
     }
 }
